5792b750eccea178c84f86ba2007346447620c6d
mgara07 <malek.gara@esprit.tn>
2022-07-14 15:38:43 +0100

    Credit & All

---
+++ softib/src/main/java/tn/esprit/softib/service/ICreditRequestService.java
@@ -0,0 +1,22 @@
+package tn.esprit.softib.service;
+
+import java.util.Set;
+
+import tn.esprit.softib.entity.Credit;
+import tn.esprit.softib.entity.CreditRequest;
+
+public interface ICreditRequestService {
+	public CreditRequest addCreditRequest(CreditRequest creditRequest, Integer id);
+	 public String deleteCreditRequest(Integer id);
+	 public String updateCreditRequest(Integer id, CreditRequest newCreditRequest) ;
+	 public CreditRequest getCreditRequest(Integer id);
+	 public CreditRequest rejectCreditRequest(Integer id);
+	 public CreditRequest acceptCreditRequestChanges(Integer id);
+	 public Credit createCreditFromCreditRequest(Integer id);
+	 public Credit mapCreditFromCreditRequest(CreditRequest creditRequest);
+	 public CreditRequest treatCreditRequest(Integer id);
+	 public CreditRequest suggestCreditRequest(CreditRequest creditRequest);
+	 public CreditRequest checkEligibaleCreditRequest(CreditRequest creditRequest);
+	 public Double calculateAmountToPayForSalary(CreditRequest creditRequest) ;
+	 public Set<CreditRequest> getAllCreditRequestAcceptedFromClients();
+}
---
+++ softib/src/main/java/tn/esprit/softib/utility/SystemDeclarations.java
@@ -0,0 +1,24 @@
+package tn.esprit.softib.utility;
+
+public class SystemDeclarations {
+	
+	//Credit
+    public static final String CREDIT_NOT_POSSIBLE= "IMPOSSIBLE CREDIT APPLICATION";
+    public static final String CREDIT_INSURANCE_NULL= "INSURANCE IS MISSING";
+    public static final String CREDIT_NOT_ENOUGH_SALARY= "SALARY NOT ENOUGH";
+    public static final String CREDIT_CONSUMPTION_AMOUNT_EXCEEDED= "MAXIMUM AMOUNT FOR CONSUMPTION CREDIT EXCEEDED";
+    public static final String CREDIT_CONSUMPTION_CREDIT_TERM_EXCEEDED= "MAXIMUM CREDIT TERM FOR CONSUMPTION CREDIT EXCEEDED";
+    public static final String CREDIT_CAR_CREDIT_TERM_EXCEEDED= "MAXIMUM CREDIT TERM FOR CAR CREDIT EXCEEDED";
+    public static final String CREDIT_HOME_CREDIT_TERM_EXCEEDED= "MAXIMUM CREDIT TERM FOR HOME CREDIT EXCEEDED";
+    
+    // Credit Variable
+    public static final Double CREDIT_INTEREST = 0.12;
+    public static final Double INSURANCE_PERCENTAGE = 0.12;
+    public static final Double CREDIT_FEES = 250d;
+    
+    // Patterns
+    public static final String DATE_FORMAT = "MM/dd/yyyy";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'hh:mm";
+    
+
+}
