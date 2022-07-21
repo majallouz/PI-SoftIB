061800fd7946b5eaed3a86417827fac51e8dc957
mgara07 <malek.gara@esprit.tn>
2022-07-21 18:42:47 +0100

    commit fix treat credit request

--- softib/src/main/java/tn/esprit/softib/service/CreditRequestServiceImpl.java
+++ softib/src/main/java/tn/esprit/softib/service/CreditRequestServiceImpl.java
@@ -157,6 +157,7 @@ public class CreditRequestServiceImpl implements ICreditRequestService {
     	} else return "Credit Request Not Found";    	
     }
 
+    
     @Override
     public Credit mapCreditFromCreditRequest(CreditRequest creditRequest) {
         Credit credit = new Credit();
@@ -200,11 +201,17 @@ public class CreditRequestServiceImpl implements ICreditRequestService {
 
     @Override
     public String treatCreditRequest(Integer id) {
+    	String msg ="";
         if (creditRequestRepository.findById(id.longValue()).isPresent()) {
             CreditRequest creditRequest = creditRequestRepository.findById(id.longValue()).get();
             creditRequest = checkEligibaleCreditRequest(creditRequest);
+            
             creditRequestRepository.save(creditRequest);
-            return ("Your credit has been "+ creditRequest.getCreditRequestStatus()+ "\n" + "   Rejection Reaseon : " + creditRequest.getRejectionReason());
+            if (creditRequest.getCreditRequestStatus().equals(CreditStatus.VALIDATED)) {
+            	msg = createCreditFromCreditRequest(id);
+            	
+            }
+            return ("Your credit has been "+ creditRequest.getCreditRequestStatus()+ "\n" + msg + "\n" +"   Rejection Reaseon : " + creditRequest.getRejectionReason());
 
         }
         return "Credit Request Not Found";
@@ -227,32 +234,35 @@ public class CreditRequestServiceImpl implements ICreditRequestService {
             if (rejectionReason.length() > 0) {
                 rejectionReason.append(", ");
             }
-            rejectionReason.append(SystemDeclarations.CREDIT_NOT_ENOUGH_SALARY);
+            rejectionReason.append(SystemDeclarations.CREDIT_NOT_ENOUGH_SALARY+" because the Amount To Pay For Salary: "+  Math.round(calculateAmountToPayForSalary(creditRequest))  );
             check = false;
         }
-    }
+    } 
         if (creditRequest.getType()!= null) {
         if (creditRequest.getType().toString().equals(TypeCredit.CONSUMPTION.toString())) {
-            if (creditRequest.getCreditAmount() > 20000) {
+        	 if (creditRequest.getCreditAmount() != null) {
+        	if (creditRequest.getCreditAmount() > 20000) {
                 check = false;
                 if (rejectionReason.length() > 0) {
                     rejectionReason.append(", ");
                 }
                 rejectionReason.append(SystemDeclarations.CREDIT_CONSUMPTION_AMOUNT_EXCEEDED);
-            }
+            }}
+            if (creditRequest.getCreditTerm() != null) {
             if (creditRequest.getCreditTerm() > 36) {
                 check = false;
                 if (rejectionReason.length() > 0) {
                     rejectionReason.append(", ");
                 }
                 rejectionReason.append(SystemDeclarations.CREDIT_CONSUMPTION_CREDIT_TERM_EXCEEDED);
-            }
+            }}
         }
         
         
         
         if (creditRequest.getType().toString().equals(TypeCredit.CAR.toString())) {
-            if (creditRequest.getCreditTerm() > 87) {
+        	 if (creditRequest.getCreditTerm() != null) {
+        	if (creditRequest.getCreditTerm() > 87) {
                 check = false;
                 if (rejectionReason.length() > 0) {
                     rejectionReason.append(", ");
@@ -260,16 +270,18 @@ public class CreditRequestServiceImpl implements ICreditRequestService {
                 rejectionReason.append(SystemDeclarations.CREDIT_CAR_CREDIT_TERM_EXCEEDED);
             }
         }
+        }
         
         
         if (creditRequest.getType().toString().equals(TypeCredit.HOME.toString())) {
-            if (creditRequest.getCreditTerm() > 300) {
+        	 if (creditRequest.getCreditTerm() != null) {
+        	if (creditRequest.getCreditTerm() > 300) {
                 check = false;
                 if (rejectionReason.length() > 0) {
                     rejectionReason.append(", ");
                 }
                 rejectionReason.append(SystemDeclarations.CREDIT_HOME_CREDIT_TERM_EXCEEDED);
-            }
+            }}
         }
         
         }
